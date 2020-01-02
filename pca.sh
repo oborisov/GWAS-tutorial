55a304925c7e61cf4e8f5bcace2ece045928ec3e
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-02 17:58:03 +0100

    Update pca.sh

--- pca.sh
+++ pca.sh
@@ -1,9 +1,25 @@
 # performing pca with plink2
 # visualizing 2 first principal components
-# identifying strong outliers
 plink_pca () {
   bfile=$1
   plink2 --bfile ${bfile} \
-  --allow-no-sex \
-  --pca --out ${bfile}
+  --pca \
+  --out ${bfile}
 }
+
+# identifying strong outliers - calculating percentiles of principal component 1 for every sample
+echo '
+library(ggplot2)
+myargs=commandArgs[1]
+eigenvec_file=fread(myargs)
+p=ggplot(eigenvec_file, aes(x=PC1, y=PC2, label=IID))+
+geom_point()
+'
+
+outliers_percentiles () {
+  eigenvec_file=$1
+  
+}
+```R
+```
+
