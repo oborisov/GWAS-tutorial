6669005c8dacebd10f12fe7a830567c5842dc1e1
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-02 11:36:23 +0100

    Update relatedness.sh

--- relatedness.sh
+++ relatedness.sh
@@ -1,6 +1,8 @@
 # from http://people.virginia.edu/~wc9c/KING/manual.html
 # the script looks and filter out 2nd degree relatives
+# make sure that family ids are not identical (e.g., "0" for all samples)
 king_relatedness () {
+  bfile=$1
   king -b ${bfile}.bed \
   --related \
   --degree 2 \
