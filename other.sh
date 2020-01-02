beb847b40d20807506e768ccd538ac776c8fa048
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-02 11:26:46 +0100

    Create other.sh

---
+++ other.sh
@@ -0,0 +1,9 @@
+exclude_ambigous_AT_GC () {
+  bfile=$1
+  plink --bfile ${bfile} \
+  --exclude <(awk '{if ($5 == "A" && $6 == "T" || $5 == "T" && $6 == "A" || $5 == "C" && $6 == "G" || $5 == "G" && $6 == "C") print $2}' ${bfile}.bim) \
+  --allow-no-sex \
+  --make-bed --out ${bfile}_noambig
+}
+# heterozygosity rate - to be added
+
