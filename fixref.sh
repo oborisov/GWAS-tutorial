152c715974a25813dbae3fab9622b6d3137a2970
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-04 16:21:48 +0100

    Update fixref.sh

--- fixref.sh
+++ fixref.sh
@@ -8,24 +8,26 @@ for i in {1..26}; do
 split_chr
 done
 
+
 # Processing autosomes
 # coverting alleles to plus strand using +fixref bcftools
 # sorting files by coordinates
 # removing duplicated variants (bcftools)
-export BCFTOOLS_PLUGINS=/home/borisov/.conda/envs/borisov_env/bcftools-1.9/plugins/
 
 bcftools_fixref_sort_rm_dup () {
   bfile=$1
-  export_BCFTOOLS_PLUGINS="$2"
+  path_to_BCFTOOLS_PLUGINS=$2
   reference_fasta=$3
   common_variants=$4
-  bcftools +fixref ${bfile}_chr${i}.vcf -Ov -o ${bfile}_flipped_chr${i}.vcf -- -f ${reference_fasta} -m flip -d
-  bcftools +fixref ${bfile}_flipped_chr${i}.vcf -Ov -o ${bfile}_ref_unsorted_chr${i}.vcf -- -d -f ${reference_fasta} -i ${common_variants}
-  bcftools sort ${bfile}_ref_unsorted_chr${i}.vcf -Ov -o ${bfile}_ref_dup_chr${i}.vcf
-  bcftools norm ${bfile}_ref_dup_chr${i}.vcf --rm-dup all -o ${bfile}_ref_chr${i}.vcf
+  export BCFTOOLS_PLUGINS=$2
+  for chr in {1..22}; do
+    bcftools +fixref ${bfile}_chr${chr}.vcf -- -f ${reference_fasta} -m flip -d | bcftools +fixref -- -d -f ${reference_fasta} -i ${common_variants} | bcftools sort | bcftools norm --rm-dup all -o ${bfile}_ref_chr${chr}.vcf &
+  done
 }
 
+bcftools_fixref_sort_rm_dup \
+  /home/borisov/ACE/ACE_HNR_full_QC_noGSA_rs78130450 \
+  /home/borisov/.conda/envs/borisov_env/bcftools-1.9/plugins/ \
+  /home/borisov/software/human_g1k_v37.fasta \
+  /home/borisov/software/common_all_20180423.vcf.gz
 
-for i in {1..22}; do
-  bcftools_fixref_sort_rm_dup ${bfile} /home/borisov/software/human_g1k_v37.fasta /home/borisov/software/common_all_20180423.vcf.gz
-done
