9b8de885fa4e0ed3a2c35d20ea6b032861e07ed0
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-05 17:39:43 +0100

    Update fixref.sh

--- fixref.sh
+++ fixref.sh
@@ -7,26 +7,27 @@
 %%bash
 ## Assign path to bfile
 bfile=
+## Reassign default variables if run in other account / on other machine
+bcftools_plugins="/home/borisov/.conda/envs/borisov_env/bcftools-1.9/plugins/"
+reference_fasta="/home/borisov/software/human_g1k_v37.fasta"
 # the following run automatically
 echo '#!/bin/bash
 bfile=$1
-path_to_BCFTOOLS_PLUGINS=$2
-reference_fasta=$3
-common_variants=$4
-chr=$5
-export BCFTOOLS_PLUGINS=$2
+reference_fasta=$2
+export $3
+chr=$4
 echo "fixref chromosome ${chr}"
 plink --bfile ${bfile} --chr ${chr} --recode vcf --out ${bfile}_chr${chr}
 bcftools +fixref ${bfile}_chr${chr}.vcf -- -f ${reference_fasta} -m flip -d | bcftools sort | bcftools norm --rm-dup all -o ${bfile}_ref_chr${chr}.vcf
-' > ~/fixref.sh
+' > ${bfile}_fixref.sh
 for chr in {1..22}; do
-sbatch --parsable --mem=16G --time=05:00:00 --cpus-per-task=8 ~/fixref.sh \
+sbatch --parsable --mem=16G --time=05:00:00 --cpus-per-task=8 \
+    ${bfile}_fixref.sh \
     ${bfile} \
-    /home/borisov/.conda/envs/borisov_env/bcftools-1.9/plugins/ \
-    /home/borisov/software/human_g1k_v37.fasta \
-    /home/borisov/software/common_all_20180423.vcf.gz \
-    ${chr} >> ~/jobs.list
+    ${reference_fasta} \
+    ${bcftools_plugins} \
+    ${chr} >> ${bfile}_jobs.list
 done
-squeue | grep -wFf ~/jobs.list > /dev/null
-while [ $? -ne 1 ]; do sleep 5; squeue | grep -wFf ~/jobs.list > /dev/null; done
+squeue | grep -wFf ${bfile}_jobs.list > /dev/null
+while [ $? -ne 1 ]; do sleep 5; squeue | grep -wFf ${bfile}_jobs.list > /dev/null; done
 echo "fixref done"
