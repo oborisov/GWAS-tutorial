ac1adab184aaddbf1be6f269b8ddeff31b44659e
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-05 23:22:20 +0100

    Update imputation.sh

--- imputation.sh
+++ imputation.sh
@@ -5,13 +5,19 @@ my_superpopulation=EUR # EUR, AMR, SAS, EAS, AFR
 chunk_size=5000000
 ## Reassign default variables if run in other account / on other machine
 dir1000genomes=/home/borisov/software/1000GP_Phase3/
+n_threads=16
 echo ${my_superpopulation} > ${bfile}_group.list
 # the following run automatically
-echo '
-  rm ${bfile}_ref_chr${i}.phased.impute2 2>/dev/null
-  rm ${bfile}_ref_chr${i}.phased.impute2_info 2>/dev/null
-  chromosome_min_coord=`tail -n +2 ${dir1000genomes}/genetic_map_chr${chr}_combined_b37.txt | head -1 | awk '{print $1}'`
-  chromosome_max_coord=`tail -n -1 ${dir1000genomes}/genetic_map_chr${chr}_combined_b37.txt | awk '{print $1}'`
+echo '#!/bin/bash
+  bfile=$1
+  dir1000genomes=$2
+  chr=$3
+  echo "imputing chromosome ${chr}"
+
+  rm ${bfile}_ref_chr${chr}.phased.impute2 2>/dev/null
+  rm ${bfile}_ref_chr${chr}.phased.impute2_info 2>/dev/null
+  chromosome_min_coord=`tail -n +2 ${dir1000genomes}/genetic_map_chr${chr}_combined_b37.txt | head -1 | cut -d " " -f 1`
+  chromosome_max_coord=`tail -n -1 ${dir1000genomes}/genetic_map_chr${chr}_combined_b37.txt | cut -d " " -f 1`
   
   for lower in `seq ${chromosome_min_coord} ${chunk_size} ${chromosome_max_coord}`; do
     # calculate upper chunk coordinate
@@ -27,6 +33,12 @@ echo '
     -Ne 20000 \
     -o ${bfile}_ref_chr${chr}.phased.impute2.${upper} \
     -filt_rules_l "${my_superpopulation}<0.01" "TYPE==LOWCOV"
+
+    done
+    wait
+  for lower in `seq ${chromosome_min_coord} ${chunk_size} ${chromosome_max_coord}`; do
+    # calculate upper chunk coordinate
+    upper=`echo ${lower} + ${chunk_size} | bc`
     # concatenate .gen imputed files to whole chromosome
     cat ${bfile}_ref_chr${chr}.phased.impute2.${upper} >> \
     ${study_dir}/${bfile}_ref_chr${chr}.phased.impute2
@@ -53,8 +65,7 @@ sbatch --parsable --partition=medium --time=24:00:00 --cpus-per-task ${n_threads
     ${bfile}_imputation.sh \
     ${bfile} \
     ${dir1000genomes} \
-    ${chr} \
-    ${n_threads} >> ${bfile}_jobs.list
+    ${chr} >> ${bfile}_jobs.list
     sleep 1
 done
 
