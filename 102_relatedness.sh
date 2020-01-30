0098060e75897f3d495a8893fc5bb7b289d655a4
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-30 11:18:59 +0100

    Update 102_relatedness.sh

--- 102_relatedness.sh
+++ 102_relatedness.sh
@@ -2,13 +2,14 @@
 # the script looks and filter out 2nd degree relatives
 # make sure that family ids are not identical (e.g., "0" for all samples)
 # if needed, change FIDs to IIDs
+%%bash
+bfile=""
 cp ${bfile}.fam \
 ${bfile}.famBK
 awk '{print $2,$2,$3,$4,$5,$6}' \
 ${bfile}.famBK > \
 ${bfile}.fam
 
-%%bash
 king_relatedness () {
   bfile=$1
   king -b ${bfile}.bed \
@@ -17,8 +18,10 @@ king_relatedness () {
   --prefix ${bfile} \
   --cpus 22
 }
-bfile="/home/borisov/LUTO/Dutch/dcg_checkedsex_geno02_mind02_geno002_mind002"
 king_relatedness ${bfile}
+
+# if FIDs were changed to IIDs, change back famBK file
+mv ${bfile}.famBK ${bfile}.fam 
 
 ## if there are no relatives, copy files with "_norelated" suffix
 cp ${bfile}.bed ${bfile}_norelated.bed
