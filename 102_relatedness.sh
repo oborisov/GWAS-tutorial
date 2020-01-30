0657eb5163259a1c18e2459a9a8d69e68998f50d
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-30 11:14:47 +0100

    Update 102_relatedness.sh

--- 102_relatedness.sh
+++ 102_relatedness.sh
@@ -1,6 +1,14 @@
 # from http://people.virginia.edu/~wc9c/KING/manual.html
 # the script looks and filter out 2nd degree relatives
 # make sure that family ids are not identical (e.g., "0" for all samples)
+# if needed, change FIDs to IIDs
+cp ${bfile}.fam \
+${bfile}.famBK
+awk '{print $2,$2,$3,$4,$5,$6}' \
+${bfile}.famBK > \
+${bfile}.fam
+
+%%bash
 king_relatedness () {
   bfile=$1
   king -b ${bfile}.bed \
@@ -9,3 +17,10 @@ king_relatedness () {
   --prefix ${bfile} \
   --cpus 22
 }
+bfile="/home/borisov/LUTO/Dutch/dcg_checkedsex_geno02_mind02_geno002_mind002"
+king_relatedness ${bfile}
+
+## if there are no relatives, copy files with "_norelated" suffix
+cp ${bfile}.bed ${bfile}_norelated.bed
+cp ${bfile}.bim ${bfile}_norelated.bim
+cp ${bfile}.fam ${bfile}_norelated.fam
