c700ad56fa1203009bc32d050f0e0a74a62620f8
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-31 18:19:39 +0100

    Update and rename 301_snptest.sh to 302_snptest.sh

--- 302_snptest.sh
+++ 302_snptest.sh
@@ -4,7 +4,7 @@ sample="_geno02_mind02_geno002_mind002_norelated_pca_ref.phased.sample"
 gen_prefix='geno02_mind02_geno002_mind002_norelated_pca_ref_chr${chr}.phased.impute2'
 for chr in {1..22}; do
     eval gen=${gen_prefix}
-    salloc srun --job-name ${job_name} --mem=1000M --partition=medium --time=24:00:00 --cpus-per-task=1 > ${gen}_imputedPC.out_slurm 2>&1 snptest \
+    salloc --job-name ${job_name} srun --mem=1000M --partition=medium --time=24:00:00 --cpus-per-task=1 > ${gen}_imputedPC.out_slurm 2>&1 snptest \
     -data ${gen}.gz ${sample} \
     -pheno case_control \
     -frequentist 1 \
