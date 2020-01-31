2dc54e9f428e232deb703385d36a1c36f55e389a
oborisov <45593402+oborisov@users.noreply.github.com>
2020-01-31 18:14:24 +0100

    Update 301_snptest.sh

--- 301_snptest.sh
+++ 301_snptest.sh
@@ -1,10 +1,10 @@
 %%bash
 job_name='snptest_'
-sample=""
-gen_prefix='_geno02_mind02_geno002_mind002_norelated_pca_ref_chr${chr}.phased.impute2'
-myfun () {
+sample="_geno02_mind02_geno002_mind002_norelated_pca_ref.phased.sample"
+gen_prefix='geno02_mind02_geno002_mind002_norelated_pca_ref_chr${chr}.phased.impute2'
+for chr in {1..22}; do
     eval gen=${gen_prefix}
-    salloc --job-name ${job_name} --mem=1000M --partition=medium --time=24:00:00 --cpus-per-task=1 > ${gen}.out_slurm 2>&1 snptest \
+    salloc srun --job-name ${job_name} --mem=1000M --partition=medium --time=24:00:00 --cpus-per-task=1 > ${gen}_imputedPC.out_slurm 2>&1 snptest \
     -data ${gen}.gz ${sample} \
     -pheno case_control \
     -frequentist 1 \
@@ -13,10 +13,7 @@ myfun () {
     -hwe \
     -missing_code NA \
     -assume_chromosome $chr \
-    -o ${gen}.out
-}
-for chr in {1..22}; do
-myfun &
+    -o ${gen}_imputedPC.out &
 done
 #sstatus=$(sacct --format="JobName%30, State" | grep snptest_LKG_2010 | awk '{print $2}' | sort | uniq -c | awk '{print $2}')
 #if [ ${sstatus} == "RUNNING" ]; then echo eq; f
