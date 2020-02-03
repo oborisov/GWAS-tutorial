e49b39c99624bad1e9676ea8259f477f7caf7c0a
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-03 11:27:33 +0100

    Update 1002_check_sex.sh

--- 1002_check_sex.sh
+++ 1002_check_sex.sh
@@ -1,7 +1,20 @@
 %%bash
+bfile=""
 sex_check () {
   bfile=$1
   plink --bfile ${bfile} \
   --check-sex --out ${bfile}_sexcheck
 }
-sex_check >/dev/null 2>&1
+sex_check ${bfile}
+
+%%R
+bfile=""
+bfile=fread(paste0(bfile, "_sexcheck.sexcheck"))
+bfile[STATUS != "OK"]
+
+%%bash
+bfile=""
+plink --bfile ${bfile} \
+--update-sex <(awk '{if ($5 == "PROBLEM") print $1,$2,$4}' ${bfile}_sexcheck.sexcheck) \
+--make-bed --out ${bfile}_checkedsex
+
