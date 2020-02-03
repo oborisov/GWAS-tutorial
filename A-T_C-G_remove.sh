b64c09e852554a4968267ead36e2a21ea434ca28
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-03 12:11:08 +0100

    Update A-T_C-G_remove.sh

--- A-T_C-G_remove.sh
+++ A-T_C-G_remove.sh
@@ -1,7 +1,8 @@
+bfile=""
 exclude_ambigous_AT_GC () {
-  bfile=$1
   plink --bfile ${bfile} \
   --exclude <(awk '{if ($5 == "A" && $6 == "T" || $5 == "T" && $6 == "A" || $5 == "C" && $6 == "G" || $5 == "G" && $6 == "C") print $2}' ${bfile}.bim) \
   --allow-no-sex \
   --make-bed --out ${bfile}_noambig
 }
+exclude_ambigous_AT_GC ${bfile}
