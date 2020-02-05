c1ca083295bfd1acc78f9610a690383f3d361622
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-05 15:47:32 +0100

    Create converting_strand.sh

---
+++ converting_strand.sh
@@ -0,0 +1,16 @@
+## Converting the chip
+
+Strand: TOP  
+
+Strand files:
+Script: https://www.well.ox.ac.uk/~wrayner/strand/update_build.sh  
+TOP: https://www.well.ox.ac.uk/~wrayner/strand/index.html  
+SRC: https://www.well.ox.ac.uk/~wrayner/strand/sourceStrand/index.html  
+
+%%bash
+bfile=""
+strand_file="GSAMD-24v1-0_20011747_A1"
+bash /home/borisov/software/update_build.sh \
+${bfile} \
+/home/borisov/software/${strand_file}-b37.strand \
+${bfile}_forward
