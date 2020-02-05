198c2321de51d606fa2b1115d9ce4baefeeeb07e
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-05 17:31:26 +0100

    Update 1002_check_sex.sh

--- 1002_check_sex.sh
+++ 1002_check_sex.sh
@@ -1,4 +1,5 @@
 %%bash
+# checking sex of samples using X-chromosome
 bfile=""
 plink --bfile ${bfile} --indep-pairwise 1000 50 0.2 --out ${bfile}_pruning
 plink --bfile ${bfile} --extract <(cat ${bfile}_pruning.prune.in) \
@@ -7,11 +8,13 @@ plink --bfile ${bfile}_pruning --check-sex --out ${bfile}_sexcheck
 rm ${bfile}_pruning*
 
 %%R
+# listing samples with incorrect sex assignment
 bfile=""
 bfile=fread(paste0(bfile, "_sexcheck.sexcheck"))
 bfile[STATUS != "OK"]
 
 %%bash
+# Updating sex using SNPSEX (determined by X-chromosome
 bfile=""
 plink --bfile ${bfile} \
 --update-sex <(awk '{if ($5 == "PROBLEM") print $1,$2,$4}' ${bfile}_sexcheck.sexcheck) \
