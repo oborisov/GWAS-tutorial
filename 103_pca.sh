25bab0affd07488f251a67f178cdad4ff1df7005
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-05 17:39:23 +0100

    Update 103_pca.sh

--- 103_pca.sh
+++ 103_pca.sh
@@ -37,15 +37,17 @@ geom_label_repel(data=eigenvec[IID %in% sd_iids])
 fwrite(eigenvec[IID %in% sd_iids][,c(2,1)], paste0(bfile, "_eigen.rm"), col.names=F, sep=" ")
 eigenvec[IID %in% sd_iids][,c(1,2,13,14)]
 
-# if there are no pc outliers, copy files with "_pca" suffix
 %%bash
-bfile=""
-cp ${bfile}.bed ${bfile}_pca.bed
-cp ${bfile}.bim ${bfile}_pca.bim
-cp ${bfile}.fam ${bfile}_pca.fam
-# or remove pc outliers (using _eigen.rm)
-%%bash
+# removing pc outliers (using _eigen.rm)
 bfile=""
 plink --bfile ${bfile} \
 --remove <(grep -wFf ${bfile}_eigen.rm ${bfile}.fam) \
 --make-bed --out ${bfile}_pca
+
+
+%%bash
+# if there are no pc outliers, copy files with "_pca" suffix
+bfile=""
+cp ${bfile}.bed ${bfile}_pca.bed
+cp ${bfile}.bim ${bfile}_pca.bim
+cp ${bfile}.fam ${bfile}_pca.fam
