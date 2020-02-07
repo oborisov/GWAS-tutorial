ef6d5710f24484936bba2247e4a49a7fbb1fed8d
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-07 16:18:07 +0100

    Update 302_snptest.sh

--- 302_snptest.sh
+++ 302_snptest.sh
@@ -13,7 +13,7 @@ for chr in {1..22}; do
     -hwe \
     -missing_code NA \
     -assume_chromosome $chr \
-    -o ${gen}_imputedPC.out &
+    -o ${gen}_imputedPC.out.gz &
 done
 #sstatus=$(sacct --format="JobName%30, State" | grep snptest_LKG_2010 | awk '{print $2}' | sort | uniq -c | awk '{print $2}')
 #if [ ${sstatus} == "RUNNING" ]; then echo eq; f
