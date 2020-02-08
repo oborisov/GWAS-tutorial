71ecb3bfefe7c32ba7dac70132d2e199614fab73
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-08 21:49:09 +0100

    Update 301_pc_after_imputation.sh

--- 301_pc_after_imputation.sh
+++ 301_pc_after_imputation.sh
@@ -1,30 +1,39 @@
 %%bash
 # Merging chromosomes
-fam="_checkedsex_geno02_mind02_geno002_mind002_norelated_pca.fam"
-sample="_geno02_mind02_geno002_mind002_norelated_pca_ref_chr1.phased.sample"
-gen_prefix='_geno02_mind02_geno002_mind002_norelated_pca_ref_chr${chr}.phased.impute2'
+bfile=""
 
 # the following run automatically
-sample_out=$(echo ${sample} | sed 's/_chr1//')
+fam=${bfile}.fam
+sample=$(echo ${bfile}_ref_chr1.phased.sample | sed 's/_chr1//')
+gen_prefix=${bfile}'_ref_chr${chr}.phased.impute2'
 rm ${sample}_concat 2> /dev/null
 for chr in {1..22}; do
 eval gen=${gen_prefix}
-plink2 --gen ${gen}.gz --sample ${sample} \
+plink2 --gen ${gen}.gz --sample ${bfile}_ref_chr1.phased.sample \
 --oxford-single-chr ${chr} \
 --update-sex <(paste -d ' ' <(awk '{print $1,$2}' $fam | tr " " "_" | awk '{print $1,$1}') <(awk '{print $5}' $fam)) \
 --pheno <(paste -d ' ' <(awk '{print $1,$2}' $fam | tr " " "_" | awk '{print $1,$1}') <(awk '{print $6}' $fam)) \
---make-bed --out ${gen} &
+--extract <(zcat ${gen}_info.gz | awk '{if ($7 > 0.8) print $2}') \
+--make-bed --out ${gen} > /dev/null 2>&1 &
 echo ${gen} >> ${sample}_concat
 done
 wait
 
-# merging all chromosomes into a single file
-salloc --mem=32000M --time=5:00:00 \
+salloc --mem=32000M --time=5:00:00 --cpus-per-task=1 \
 srun plink --merge-list ${sample}_concat \
 --out ${sample}_concat
+#######################################################
 
+%%bash
+bfile=""
+
+# the following run automatically
+fam=${bfile}.fam
+sample=$(echo ${bfile}_ref_chr1.phased.sample | sed 's/_chr1//')
+gen_prefix=${bfile}'_ref_chr${chr}.phased.impute2'
 # cleaning from temp plink per-chromosome files
 for chr in {1..22}; do eval gen=${gen_prefix}
+eval gen=${gen_prefix}
 rm ${gen}.bed ${gen}.bim ${gen}.fam ${gen}.log
 done; rm ${sample}_concat
 
@@ -37,15 +46,17 @@ plink --bfile ${bfile} \
 --make-bed --out ${bfile}_pruned
 salloc --mem=16000M --time=5:00:00 --cpus-per-task=20 \
 srun plink2 --bfile ${bfile}_pruned --pca --out ${bfile}_eigen
-rm ${bfile}_pruned*
-cat ${bfile}_eigen.eigenval
+#######################################################
 
 %%R
 # visualizing 2 first principal components
-library(ggrepel)
-n_sd=6
 bfile=""
+bfile=paste0(bfile, "_ref.phased.sample_concat")
+n_sd=6
+system(paste0("rm ", bfile, "_pruned*"))
+library(ggrepel)
 eigenvec=fread(paste0(bfile, "_eigen.eigenvec"))
+eigenval=fread(paste0(bfile, "_eigen.eigenval"), header=F)
 fam=fread(paste0(bfile, ".fam"), header=F)
 colnames(fam)[6]="cc_status"
 eigenvec=merge(eigenvec, fam[,c(2,6)], by.x="IID", by.y="V2")
@@ -57,6 +68,7 @@ for (x in 3:4) {
 }
 sd_iids=eigenvec[sd_for_PC1 > n_sd | sd_for_PC2 > n_sd]$IID
 ggplot(eigenvec, aes(x=PC1, y=PC2, color=cc_status, label = IID))+
-geom_point() + # color = ifelse(eigenvec$IID %in% sd_iids, "red", "grey50")
-geom_label_repel(data=eigenvec[IID %in% sd_iids])
-
+geom_point() +
+geom_label_repel(data=eigenvec[IID %in% sd_iids]) +
+ggtitle(paste0("PC1=", eigenval[1], " PC2=", eigenval[2], " PC3=", eigenval[3], " PC4=", eigenval[4]))
+#######################################################
