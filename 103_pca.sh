c0cf858fed17201ed91ee0c89f66f418bb576f02
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-08 11:30:43 +0100

    Update 103_pca.sh

--- 103_pca.sh
+++ 103_pca.sh
@@ -9,15 +9,15 @@ plink --bfile ${bfile} \
 --make-bed --out ${bfile}_pruned
 salloc --mem=16000M --time=5:00:00 --cpus-per-task=20 \
 srun plink2 --bfile ${bfile}_pruned --pca --out ${bfile}_eigen
-rm ${bfile}_pruned*
-cat ${bfile}_eigen.eigenval
 
 %%R
 # visualizing 2 first principal components
 bfile=""
 n_sd=6
+system(paste0("rm ", bfile, "_pruned*"))
 library(ggrepel)
 eigenvec=fread(paste0(bfile, "_eigen.eigenvec"))
+eigenval=fread(paste0(bfile, "_eigen.eigenval"), header=F)
 fam=fread(paste0(bfile, ".fam"), header=F)
 colnames(fam)[6]="cc_status"
 eigenvec=merge(eigenvec, fam[,c(2,6)], by.x="IID", by.y="V2")
@@ -29,9 +29,9 @@ for (x in 3:4) {
 }
 sd_iids=eigenvec[sd_for_PC1 > n_sd | sd_for_PC2 > n_sd]$IID
 ggplot(eigenvec, aes(x=PC1, y=PC2, color=cc_status, label = IID))+
-geom_point() + # color = ifelse(eigenvec$IID %in% sd_iids, "red", "grey50")
-geom_label_repel(data=eigenvec[IID %in% sd_iids])
-
+geom_point() +
+geom_label_repel(data=eigenvec[IID %in% sd_iids]) +
+ggtitle(paste0("PC1=", eigenval[1], " PC2=", eigenval[2], " PC3=", eigenval[3], " PC4=", eigenval[4]))
 
 %%R
 # More than n_sd SD outliers based on PC1 and PC2:
@@ -44,7 +44,7 @@ bfile=""
 plink --bfile ${bfile} \
 --remove <(grep -wFf ${bfile}_eigen.rm ${bfile}.fam) \
 --make-bed --out ${bfile}_pca
-
+rm ${bfile}_eigen.rm
 
 %%bash
 # if there are no pc outliers, copy files with "_pca" suffix
