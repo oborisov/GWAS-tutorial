15007867017e5cb43b1a7db105cafdd24346ac41
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-09 18:31:49 +0100

    Update 303_qqman.sh

--- 303_qqman.sh
+++ 303_qqman.sh
@@ -1,19 +1,16 @@
 %%R
-# set up 4 variables
-path_to_wd="/home/borisov/"
-pattern_of_snptest=".out.gz$"
+# set up 3 variables
+bfile=""
 info_threshold=0.8
 maf_threshold=0.01
 
-## the rest should run automatically
-
-# xcat default is bitmapType; # getOption("bitmapType"); # [1] "Xlib"; # but it requires X11 which is not present; # to overcome, use cairo graphics (https://github.com/easybuilders/easybuild-easyconfigs/issues/4918); options(bitmapType="cairo")
+# the rest will run automatically
 
 # libraries
 lapply(c("qqman", "QCEWAS", "RColorBrewer"), require, character.only = TRUE)
 
 # reading snptest output into dt
-snptest_out_list_files=list.files(path=path_to_wd, pattern=pattern_of_snptest, full.names=T)
+snptest_out_list_files=list.files(path=gsub("(.*/).*","\\1",bfile), pattern=paste0(gsub(".*/","",bfile), ".*.out.gz$"), full.names=T)
 snptest_out_dt=rbindlist(sapply(snptest_out_list_files, function(x) {
     fread(cmd=paste0("zcat ", x, " | grep -v \\#"))[,c("rsid", "chromosome", "position", "info", "cases_maf", "controls_maf", "frequentist_add_pvalue")]
 }, simplify=F))
@@ -31,12 +28,8 @@ snptest_out_dt=snptest_out_dt[!is.na(snptest_out_dt$P) & P > 0]
 lambda <- median(qchisq(1 - snptest_out_dt$P, 1)) / qchisq(0.5, 1)
 
 # producing Manhattan plot
-path_to_wd="/home/borisov/nsCLP/LKG_2010/"
-pattern_of_snptest="LKG_2010_cases_399_controls_1318_geno02_mind02_geno002_mind002_norelated_pca_ref_chr.*.phased.impute2_imputedPC.out$"
 
-plot_prefix=paste0(path_to_wd, gsub("\\$", "", gsub("_chr\\.\\*", "", pattern_of_snptest)))
-
-jpeg(paste0(plot_prefix, "_manh.jpeg"),
+jpeg(paste0(bfile, "_manh.jpeg"),
      width = 12, height = 6, units = "in", res = 600)
 print(manhattan(snptest_out_dt, chr="CHR", bp="BP", p="P", snp="SNP",
                 annotatePval = 1, annotateTop = T,
@@ -45,10 +38,10 @@ title(main = plot_prefix)
 dev.off()
 
 # producing Q-Q plot
-jpeg(paste0(plot_prefix, "_qq.jpeg"),
+jpeg(paste0(bfile, "_qq.jpeg"),
      width = 6, height = 6, units = "in", res = 600)
 print(qq(snptest_out_dt$P))
-title(main = plot_prefix,
+title(main = gsub(".*/(.*)_checkedsex.*","\\1",bfile),
       sub = paste0("Lambda=", round(lambda,4)))
 dev.off()
 
@@ -58,4 +51,3 @@ print(manhattan(snptest_out_dt, chr="CHR", bp="BP", p="P", snp="SNP",
                 annotatePval = 1, annotateTop = T,
                 col=brewer.pal(8, "Dark2")))
 print(qq(snptest_out_dt$P))
-
