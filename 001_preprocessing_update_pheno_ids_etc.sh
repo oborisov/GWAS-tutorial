10fe314d50fc528a06aa57b03472becc96057a02
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-10 17:39:13 +0100

    Update 001_preprocessing_update_pheno_ids_etc.sh

--- 001_preprocessing_update_pheno_ids_etc.sh
+++ 001_preprocessing_update_pheno_ids_etc.sh
@@ -1,1 +1,21 @@
+### --update-ids cannot be used in the same run as --update-parents or --update-sex.
+%%bash
+bfile="/home/borisov/nsCLP/LKG_2018_Sharknado/LKG_2018_Sharknado"
 
+echo "" > ${bfile}_updpheno_temp
+echo "" > ${bfile}_updsex_temp
+
+
+plink --file  \
+--make-bed --out ${bfile}
+
+plink --bfile ${bfile} \
+--pheno <(awk '{print $1,$2,2}' ${bfile}.fam) \
+--update-ids ${bfile}_updpheno_temp \
+--make-bed --out ${bfile}_updids
+
+plink --bfile ${bfile}_updids \
+--update-sex ${bfile}_updsex_temp \
+--make-bed --out ${bfile}_updids_updsex
+
+rm ${bfile}_updpheno_temp ${bfile}_updsex_temp ${bfile}_updids.*
