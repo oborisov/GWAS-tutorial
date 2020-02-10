db9d7194f08b611c0867d4d43eaaec0741f85c98
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-10 13:45:03 +0100

    Update 302_snptest.sh

--- 302_snptest.sh
+++ 302_snptest.sh
@@ -1,3 +1,21 @@
+%%bash
+### Computing lambda
+bfile=""
+# plink logistic with 4 pc
+bfile="${bfile}_ref.phased.sample_concat"
+plink --bfile ${bfile} \
+--logistic \
+--covar ${bfile}_eigen.eigenvec \
+--covar-name PC1, PC2, PC3, PC4 \
+--out ${bfile}_lambda
+# select p values for phenotype only
+awk '{if ($5 == "ADD") print $9}' ${bfile}_lambda.assoc.logistic > ${bfile}_lambda_P
+# remove temp files
+rm ${bfile}_lambda.assoc.logistic
+# Computing lambda
+Rscript -e 'bfile=commandArgs(T); dt=data.table(lambda_4pc=median(qchisq(1 - fread(paste0(bfile, "_lambda_P"), header=F)[[1]], 1), na.rm=T) / qchisq(0.5, 1)); fwrite(dt, paste0(bfile, "_lambda_P"); dt)' ${bfile} > ${bfile}_lambda_P
+#####################################################
+
 %%R
 #creating sample with PCs
 bfile=""
@@ -37,7 +55,7 @@ for chr in {1..22}; do
     -pheno case_control \
     -frequentist 1 \
     -method expected \
-    -cov_all_continuous \
+    -cov_names PC1 PC2 PC3 PC4 \
     -hwe \
     -missing_code NA \
     -assume_chromosome $chr \
