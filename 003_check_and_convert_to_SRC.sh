91fff3cbdb8f30e7cda5795a0eed3470e77becd2
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-22 15:11:38 +0100

    Update 003_check_and_convert_to_SRC.sh

--- 003_check_and_convert_to_SRC.sh
+++ 003_check_and_convert_to_SRC.sh
@@ -7,8 +7,8 @@ Probability of strand match 100
 %%bash
 bfile=""
 strand_file="GSAMD-24v1-0_20011747_A1"
-strand_file=${strand_file}-b37
+strand_file=${strand_file}-b37 # add .Source here if SRC
 bash /home/borisov/software/update_build.sh \
 ${bfile} \
-/home/borisov/software/manifest_strand/${strand_file}-b37.strand \
+/home/borisov/software/manifest_strand/${strand_file}.strand \
 ${bfile}_SRC
