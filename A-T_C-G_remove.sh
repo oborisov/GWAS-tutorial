ee4f5b893c59080bddd1c2331be1b24a2265d4f4
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-22 18:03:28 +0100

    Update A-T_C-G_remove.sh

--- A-T_C-G_remove.sh
+++ A-T_C-G_remove.sh
@@ -1,5 +1,17 @@
 %%bash
-##### Remove ambigous variants after merging
+# merge 2 data sets
+bfile1=""
+bfile2=""
+plink --bfile ${bfile1} \
+--bmerge ${bfile2} \
+--out ${bfile1}_bfile2
+plink --bfile ${bfile1} \
+--flip ${bfile1}_bfile2.missnp \
+--make-bed --out ${bfile1}_filt
+plink --bfile ${bfile1}_filt \
+--bmerge ${bfile2}_Bonn \
+--out {bfile1}_bfile2
+# Remove ambigous variants after merging
 bfile=""
 plink --bfile ${bfile} \
 --exclude <(awk '{if ($5 == "A" && $6 == "T" || $5 == "T" && $6 == "A" || $5 == "C" && $6 == "G" || $5 == "G" && $6 == "C") print $2}' ${bfile}.bim) \
