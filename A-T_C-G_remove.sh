9ced335eefb313bc4846b8272cebf8ce72910dff
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-22 17:29:59 +0100

    Update A-T_C-G_remove.sh

--- A-T_C-G_remove.sh
+++ A-T_C-G_remove.sh
@@ -1,6 +1,6 @@
 %%bash
+##### Remove ambigous variants after merging
 bfile=""
 plink --bfile ${bfile} \
 --exclude <(awk '{if ($5 == "A" && $6 == "T" || $5 == "T" && $6 == "A" || $5 == "C" && $6 == "G" || $5 == "G" && $6 == "C") print $2}' ${bfile}.bim) \
 --allow-no-sex --make-bed --out ${bfile}_noambig
-exclude_ambigous_AT_GC ${bfile}
