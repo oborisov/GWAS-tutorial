ccc5bcec4b0ae73d09a01ea77d1170eb4e6483fe
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-23 15:19:33 +0100

    Update plink_glm.sh

--- plink_glm.sh
+++ plink_glm.sh
@@ -115,3 +115,22 @@ jpeg("/home/borisov/ACE/ACE_HNR_full_QC_PC3-5.PHENO1.glm.logistic_qq.jpeg", widt
 print(qq(sumstats[[5]]))
 title(main = "ACE_HNR_full_QC_PC3-5.PHENO1.glm.logistic", sub = paste0("Lambda=", round(my_lambda,4)))
 dev.off()
+
+### Lambda
+%%bash
+### Computing lambda
+bfile=""
+# plink logistic with 4 pc
+bfile="${bfile}_ref.phased.sample_concat"
+plink --bfile ${bfile} \
+--logistic \
+--covar ${bfile}_eigen.eigenvec \
+--covar-name PC1, PC2, PC3, PC4 \
+--out ${bfile}_lambda
+# select p values for phenotype only
+awk '{if ($5 == "ADD") print $9}' ${bfile}_lambda.assoc.logistic > ${bfile}_lambda_P
+# remove temp files
+rm ${bfile}_lambda.assoc.logistic
+# Computing lambda
+Rscript -e 'bfile=commandArgs(T); dt=data.table(lambda_4pc=median(qchisq(1 - fread(paste0(bfile, "_lambda_P"), header=F)[[1]], 1), na.rm=T) / qchisq(0.5, 1)); system(paste0("rm ", paste0(bfile, "_lambda_P"))); fwrite(dt, paste0(bfile, "_lambda")); dt' ${bfile} > ${bfile}_lambda
+#####################################################
