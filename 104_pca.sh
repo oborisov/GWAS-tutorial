51cba4c0cf7ce0fe39c107351a636f082aee5266
oborisov <45593402+oborisov@users.noreply.github.com>
2020-02-28 11:30:16 +0100

    Update 104_pca.sh

--- 104_pca.sh
+++ 104_pca.sh
@@ -44,7 +44,6 @@ bfile=""
 plink --bfile ${bfile} \
 --remove <(grep -wFf <(awk '{print $2}' ${bfile}_eigen.rm) ${bfile}.fam) \
 --make-bed --out ${bfile}_pca
-rm ${bfile}_eigen.rm
 
 %%bash
 # if there are no pc outliers, copy files with "_pca" suffix
