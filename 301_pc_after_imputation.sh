6f6c1f562d841776a02ab41c792834ee27475679
oborisov <45593402+oborisov@users.noreply.github.com>
2020-03-18 16:00:01 +0100

    Update 301_pc_after_imputation.sh

--- 301_pc_after_imputation.sh
+++ 301_pc_after_imputation.sh
@@ -49,26 +49,53 @@ srun plink2 --bfile ${bfile}_pruned --pca --out ${bfile}_eigen
 #######################################################
 
 %%R
-# visualizing 2 first principal components
 bfile=""
 bfile=paste0(bfile, "_ref.phased.sample_concat")
+# PC1&PC2 plot with independent pc computed outliers
+n_PC=2
 n_sd=6
-system(paste0("rm ", bfile, "_pruned*"))
+use_SD=T
+if (use_SD) {center_fun <- mean; var_fun <- sd} else {center_fun <- median; var_fun <- IQR}
+
+# library
 library(ggrepel)
+
+# fam, eigenvec, eigenval
 eigenvec=fread(paste0(bfile, "_eigen.eigenvec"))
 eigenval=fread(paste0(bfile, "_eigen.eigenval"), header=F)
 fam=fread(paste0(bfile, ".fam"), header=F)
 colnames(fam)[6]="cc_status"
 eigenvec=merge(eigenvec, fam[,c(2,6)], by.x="IID", by.y="V2")
 eigenvec[, cc_status := as.factor(cc_status)]
-for (x in 3:4) {
-    ind=x+15
-    mycol=paste0("sd_for_PC", x-2)
-    eigenvec[, (mycol) := round(abs(eigenvec[[x]] - median(eigenvec[[x]])) / IQR(eigenvec[[x]])+0.5)]
-}
-sd_iids=eigenvec[sd_for_PC1 > n_sd | sd_for_PC2 > n_sd]$IID
-ggplot(eigenvec, aes(x=PC1, y=PC2, color=cc_status, label = IID))+
+for (x in 3:(2+n_PC)) { mycol=paste0("sd_for_PC", x-2)
+    eigenvec[, (mycol) := round(abs(eigenvec[[x]] - center_fun(eigenvec[[x]])) / var_fun(eigenvec[[x]])+0.5)] }
+column_number_vector=c(14:(13+n_PC))
+eigenvec_melt = melt(eigenvec[,c(1,13:ncol(eigenvec)), with=F], id.vars = c("IID"), 
+                     measure.vars = colnames(eigenvec[,c(14:ncol(eigenvec)), with=F]))
+print(eigenvec_melt[value > n_sd])
+
+print(ggplot(eigenvec, aes(x=PC1, y=PC2, color=cc_status, label = IID))+
 geom_point() +
-geom_label_repel(data=eigenvec[IID %in% sd_iids]) +
-ggtitle(paste0("PC1=", eigenval[1], " PC2=", eigenval[2], " PC3=", eigenval[3], " PC4=", eigenval[4]))
-#######################################################
+geom_label_repel(data=eigenvec[IID %in% eigenvec_melt[value > n_sd]$IID]) + theme_bw())
+ggsave(paste0(bfile, "_eigen.eigenvec_pca.jpeg"))
+
+# More than n_sd SD outliers based on PC1 and PC2:
+fwrite(eigenvec[IID %in% eigenvec_melt[value > n_sd]$IID][,c(2,1)], paste0(bfile, "_eigen.rm"), col.names=F, sep=" ")
+
+# Eigenvalues
+colnames(eigenval)="Eigenvalues"
+eigenval$PC=factor(paste0("PC", 1:10), levels=paste0("PC", 1:10))
+eigenval$PC_temp=as.integer(1:10)
+eigenval[, perc_var_explained := round(eigenval[[1]]*100/sum(eigenval[[1]]),1)]
+
+# Variance explained by PCs
+print(eigenval[,c(2,1,4)])
+
+# Scree plot
+print(ggplot(eigenval, aes(x=PC, y=Eigenvalues)) +
+geom_point() + geom_line(aes(x=PC_temp, y=Eigenvalues)) + theme_bw() +
+ggtitle(paste0("PC1=", eigenval[1,4], "%; PC2=", eigenval[2,4], "%; PC3=", eigenval[3,4], "%; PC4=", eigenval[4,4], "%")))
+ggsave(paste0(bfile, "_eigen.eigenval_scree.jpeg"))
+
+# remove temp files
+system(paste0("rm ", bfile, "_pruned*"))
