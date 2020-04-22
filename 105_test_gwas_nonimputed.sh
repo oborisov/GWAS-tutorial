cb9f207848b9631e3c83124558ff9854471f8f32
oborisov <45593402+oborisov@users.noreply.github.com>
2020-04-22 12:23:11 +0200

    Update 105_test_gwas_nonimputed.sh

--- 105_test_gwas_nonimputed.sh
+++ 105_test_gwas_nonimputed.sh
@@ -1,29 +1,26 @@
 %%bash
 bfile=""
-plink --bfile ${bfile} \
+plink2 --bfile ${bfile} \
 --covar ${bfile}_eigen.eigenvec \
---covar-name PC1, PC2, PC3, PC4 \
---logistic hide-covar \
+--covar-name PC1,PC2,PC3,PC4 \
+--glm hide-covar \
 --out ${bfile}_test_4pc
 
 %%R
 bfile=""
 lapply(c("qqman", "RColorBrewer"), require, character.only = TRUE)
-pvec_4pc=fread(paste0(bfile, "_test_4pc", ".assoc.logistic"))
+pvec_4pc=fread(paste0(bfile, "_test_4pc.PHENO1.glm.logistic"))
 print(pvec_4pc[order(P)][1:10])
 # adjusting CHR and p
-pvec_4pc[CHR == "X", CHR := 23]
-pvec_4pc[, CHR := as.numeric(CHR)]
-pvec_4pc=pvec_4pc[!is.na(pvec_4pc$P) & P > 0]
-lambda=median(qchisq(1 - pvec_4pc[[9]], 1), na.rm=T) / qchisq(0.5, 1)
+pvec_4pc[`#CHROM` == "X" | `#CHROM` == "XY", `#CHROM` := 23]
+pvec_4pc[, `#CHROM` := as.numeric(`#CHROM`)]
+pvec_4pc=pvec_4pc[!is.na(pvec_4pc$`#CHROM`) & !is.na(pvec_4pc$P) & P > 0]
+lambda=median(qchisq(1 - pvec_4pc$P, 1), na.rm=T) / qchisq(0.5, 1)
 # producing Manhattan plot
-print(manhattan(rbind(pvec_4pc[P<5e-2], pvec_4pc[P>5e-2][seq(1,nrow(pvec_4pc[P>5e-2]),10)]), chr="CHR", bp="BP", p="P", snp="SNP", annotatePval = 1, annotateTop = T, col=brewer.pal(8, "Dark2")))
+print(manhattan(rbind(pvec_4pc[P<5e-2], pvec_4pc[P>5e-2][seq(1,nrow(pvec_4pc[P>5e-2]),10)]), chr="#CHROM", bp="POS", p="P", snp="SNP", annotatePval = 1, annotateTop = T, col=brewer.pal(8, "Dark2")))
 # producing Q-Q plot
 print(qq(pvec_4pc$P))
 title(main = paste0("Lambda=", round(lambda,4)))
-bim=fread(paste0(bfile, ".bim"))
-print(dim(bim[V2 %in% pvec_4pc[P < 5e-8]$SNP]))
-print(dim(bim[V2 %in% pvec_4pc[P < 5e-8]$SNP][V5 == "T" & V6 == "A" | V5 == "A" & V6 == "T" | V5 == "C" & V6 == "G" | V5 == "G" & V6 == "C"]))
 
 %%bash
 # manual check of some SNPs
